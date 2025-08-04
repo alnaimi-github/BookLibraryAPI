0307749c4234429e97dc9ad33249799bac6fafe8
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 16:52:15 +0300

    Add LibraryDbContext for EF Core integration and include Microsoft.EntityFrameworkCore package

---
+++ src/Infrastructure/BookLibraryAPI.Infrastructure/Persistence/LibraryDbContext.cs
@@ -0,0 +1,10 @@
+﻿using BookLibraryAPI.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibraryAPI.Infrastructure.Persistence;
+
+public sealed class LibraryDbContext(DbContextOptions<LibraryDbContext> options) 
+    : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; }
+}
