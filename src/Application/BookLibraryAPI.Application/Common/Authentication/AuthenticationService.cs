c91ebe0a4ac1b5e71f45442f5870b1b37ac46b75
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 22:05:23 +0300

    Add IAuthenticationService interface for user authentication operations

---
+++ src/Application/BookLibraryAPI.Application/Common/Authentication/AuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using BookLibraryAPI.Core.Domain.Common;
+using BookLibraryAPI.Core.Domain.Users;
+
+namespace BookLibraryAPI.Application.Common.Authentication;
+
+public interface IAuthenticationService
+{
+    Task<Result<User?>> ValidateUserAsync(string username, string password, CancellationToken cancellationToken = default);
+    Result<string> HashPassword(string password);
+    Result<bool> VerifyPassword(string password, string hash);
+}
