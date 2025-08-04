14a4ce9c21e84a5369c1de6bc4892187332492fa
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 17:13:12 +0300

    Implement OnModelCreating method in LibraryDbContext to apply entity configurations

--- src/Infrastructure/BookLibraryAPI.Infrastructure/Persistence/LibraryDbContext.cs
+++ src/Infrastructure/BookLibraryAPI.Infrastructure/Persistence/LibraryDbContext.cs
@@ -7,4 +7,10 @@ public sealed class LibraryDbContext(DbContextOptions<LibraryDbContext> options)
     : DbContext(options)
 {
     public DbSet<Book> Books { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LibraryDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
