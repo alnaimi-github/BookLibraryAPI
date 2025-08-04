f61989ff396dfd69998ea25b919d17f5953e0e0f
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 16:22:31 +0300

    Add Book entity to represent book details in the domain

---
+++ src/Core/BookLibraryAPI.Core.Domain/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿using BookLibraryAPI.Core.Domain.Common;
+
+namespace BookLibraryAPI.Core.Domain.Entities;
+
+public sealed class Book : BaseEntity
+{
+    public string Title { get; private set; } = string.Empty;
+    public string Author { get; private set; } = string.Empty;
+    public int Year { get; private set; }
+    
+}
