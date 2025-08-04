336af0242b0fa0f223d99d62432fa966d8dd5435
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 17:42:01 +0300

    Add CreateBookCommand for handling book creation requests

---
+++ src/Application/BookLibraryAPI.Application/Features/Books/Commands/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,9 @@
+﻿using BookLibraryAPI.Application.Common.DTOs.Books;
+using MediatR;
+
+namespace BookLibraryAPI.Application.Features.Books.Commands.CreateBook;
+
+public record CreateBookCommand(
+    string Title,
+    string Author,
+    int Year) : IRequest<BookDto>;
