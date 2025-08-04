7ef9c5e961ced5a6c8f93502b5867ce46aecaf3b
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 17:35:47 +0300

    Add BookDto record for transferring book data

---
+++ src/Application/BookLibraryAPI.Application/Common/DTOs/Books/BookDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookLibraryAPI.Application.Common.DTOs.Books;
+
+public sealed record BookDto(
+    int Id, 
+    string Title,
+    string Author,
+    int Year,
+    DateTime CreatedAt);
