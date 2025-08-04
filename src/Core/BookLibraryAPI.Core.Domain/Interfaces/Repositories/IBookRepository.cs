456533d0eb50ca30926d674c93f73c42264e8e29
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 16:45:12 +0300

    Add IBookRepository interface for book data access operations

---
+++ src/Core/BookLibraryAPI.Core.Domain/Interfaces/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookLibraryAPI.Core.Domain.Entities;
+
+namespace BookLibraryAPI.Core.Domain.Interfaces.Repositories;
+
+public interface IBookRepository
+{
+    Task<IEnumerable<Book>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Book?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<Book> AddAsync(Book book, CancellationToken cancellationToken = default);
+    Task<Book> UpdateAsync(Book book, CancellationToken cancellationToken = default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    Task<bool> ExistsAsync(int id, CancellationToken cancellationToken = default);
+}
