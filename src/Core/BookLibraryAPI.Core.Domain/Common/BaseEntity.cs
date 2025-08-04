18943e914b6d5d618525f46ea14a306ddbae314e
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-04 16:19:48 +0300

    Add BaseEntity class to define common properties for domain entities

---
+++ src/Core/BookLibraryAPI.Core.Domain/Common/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace BookLibraryAPI.Core.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+
+    protected BaseEntity()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    protected void SetUpdatedAt()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
