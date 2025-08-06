76acf7cc126ac319fa3b9c9f3e8127d2acd4d6b4
alnaimi-github <alnymybdalwas1@gmail.com>
2025-08-06 22:33:21 +0300

    Fix UpdateBookCommandValidatorTests to use correct record constructor for UpdateBookCommand

--- tests/BookLibraryAPI.UnitTests/Books/UpdateBookCommandValidatorTests.cs
+++ tests/BookLibraryAPI.UnitTests/Books/UpdateBookCommandValidatorTests.cs
@@ -11,7 +11,7 @@ public class UpdateBookCommandValidatorTests
     [Fact]
     public void Should_Have_Error_When_Title_Is_Empty()
     {
-        var command = new UpdateBookCommand { Id = 1, Title = "", Author = "Author", Year = 2020 };
+        var command = new UpdateBookCommand(1, "", "Author", 2020);
         var result = _validator.Validate(command);
         result.Errors.Should().Contain(e => e.PropertyName == "Title");
     }
@@ -19,7 +19,7 @@ public class UpdateBookCommandValidatorTests
     [Fact]
     public void Should_Have_Error_When_Author_Is_Empty()
     {
-        var command = new UpdateBookCommand { Id = 1, Title = "Title", Author = "", Year = 2020 };
+        var command = new UpdateBookCommand(1, "Title", "", 2020);
         var result = _validator.Validate(command);
         result.Errors.Should().Contain(e => e.PropertyName == "Author");
     }
@@ -27,7 +27,7 @@ public class UpdateBookCommandValidatorTests
     [Fact]
     public void Should_Have_Error_When_Year_Is_Invalid()
     {
-        var command = new UpdateBookCommand { Id = 1, Title = "Title", Author = "Author", Year = 2200 };
+        var command = new UpdateBookCommand(1, "Title", "Author", 2200);
         var result = _validator.Validate(command);
         result.Errors.Should().Contain(e => e.PropertyName == "Year");
     }
@@ -35,7 +35,7 @@ public class UpdateBookCommandValidatorTests
     [Fact]
     public void Should_Not_Have_Error_When_Command_Is_Valid()
     {
-        var command = new UpdateBookCommand { Id = 1, Title = "Valid Title", Author = "Valid Author", Year = 2024 };
+        var command = new UpdateBookCommand(1, "Valid Title", "Valid Author", 2024);
         var result = _validator.Validate(command);
         result.IsValid.Should().BeTrue();
     }
